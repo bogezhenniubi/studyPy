3940f21e8f918360e2e313fdfb055d902055a7f5
h0t3 <3541778300@qq.com>
2021-02-28 11:11:33 +0800

    day06

---
+++ day06/main(1).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void value(int b)
+{
+	b=2;
+	cout<<b<<endl;
+ } 
+int main()
+{
+    int a=1;
+    cout<<a<<endl;
+    value(a);
+    cout<<a<<endl;   
+	return 0;       
+}
